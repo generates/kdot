ac554cab35f4133adbbd27ff6c28372dba7f2d2f
Ian Walter <pub@ianwalter.dev>
2021-01-23 16:12:06 -0500

    Fixing isDependency logic

--- packages/kdot/lib/configure/index.js
+++ packages/kdot/lib/configure/index.js
@@ -189,8 +189,9 @@ export default async function configure ({ ext, ...input }) {
   }
 
   if (cfg.resources.configMaps?.length) {
-    for (const { app, ...configMap } of cfg.resources.configMaps) {
+    for (const configMap of cfg.resources.configMaps) {
       const { name, namespace } = configMap.metadata
+      const { app } = configMap
       const { body: { items } } = await core.listNamespacedConfigMap(namespace)
       const existing = items.find(i => {
         return i.metadata.name === name && i.metadata.namespace === namespace
@@ -201,14 +202,14 @@ export default async function configure ({ ext, ...input }) {
   }
 
   if (cfg.resources.secrets?.length) {
-    for (const { app, ...secret } of cfg.resources.secrets) {
+    for (const secret of cfg.resources.secrets) {
       const { name, namespace } = secret.metadata
       const { body: { items } } = await core.listNamespacedSecret(namespace)
       const existing = items.find(i => {
         return i.metadata.name === name && i.metadata.namespace === namespace
       })
       if (existing) secret.metadata.uid = existing.metadata.uid
-      if (!existing || !app?.isDependency) cfg.resources.all.push(secret)
+      if (!existing || !secret.app?.isDependency) cfg.resources.all.push(secret)
     }
   }
 
@@ -223,25 +224,27 @@ export default async function configure ({ ext, ...input }) {
 
   if (cfg.resources.deployments.length) {
     const { body: { items } } = await apps.listDeploymentForAllNamespaces()
-    for (const { app, ...deployment } of cfg.resources.deployments) {
+    for (const deployment of cfg.resources.deployments) {
       const { name, namespace } = deployment.metadata
+      const { isDependency } = deployment.app
       const existing = items.find(i => {
         return i.metadata.name === name && i.metadata.namespace === namespace
       })
       if (existing) deployment.metadata.uid = existing.metadata.uid
-      if (!existing || !app.isDependency) cfg.resources.all.push(deployment)
+      if (!existing || !isDependency) cfg.resources.all.push(deployment)
     }
   }
 
   if (cfg.resources.services.length) {
     const { body: { items } } = await core.listServiceForAllNamespaces()
-    for (const { app, ...service } of cfg.resources.services) {
+    for (const service of cfg.resources.services) {
       const { name, namespace } = service.metadata
+      const { isDependency } = service.app
       const existing = items.find(i => {
         return i.metadata.name === name && i.metadata.namespace === namespace
       })
       if (existing) service.metadata.uid = existing.metadata.uid
-      if (!existing || !app.isDependency) cfg.resources.all.push(service)
+      if (!existing || !isDependency) cfg.resources.all.push(service)
     }
   }
 
