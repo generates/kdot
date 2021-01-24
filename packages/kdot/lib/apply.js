45d60f928bc516841fca70e767efcd00c584c552
Ian Walter <pub@ianwalter.dev>
2021-01-23 19:55:59 -0500

    Fixing typos

--- packages/kdot/lib/apply.js
+++ packages/kdot/lib/apply.js
@@ -23,115 +23,116 @@ function logUpdate (resource) {
   logger.log(emojis[resource.kind] || '☸️', message)
 }
 
-async function applyResource ({ app, ...resource }) {
-  const { uid, name, namespace } = resource.metadata
-  try {
-    if (resource.kind === 'Namespace') {
-      if (!uid) {
-        await core.createNamespace(resource)
-        logger.success('Created Namespace:', name)
-      }
-    } else if (resource.kind === 'Deployment') {
-      // If the app depends on other apps, wait for the other apps to have
-      // running pods before creating the dependent app's Deployment.
-      if (app.dependsOn?.length) {
-        const toWait = name => getRunningPod(namespace, name)
-        await Promise.all(app.dependsOn.map(toWait))
-      }
+function getApplyResource (logLevel) {
+  return async function applyResource ({ app, ...resource }) {
+    const { uid, name, namespace } = resource.metadata
+    try {
+      if (resource.kind === 'Namespace') {
+        if (!uid) {
+          await core.createNamespace(resource)
+          logger.success('Created Namespace:', name)
+        }
+      } else if (resource.kind === 'Deployment') {
+        // If the app depends on other apps, wait for the other apps to have
+        // running pods before creating the dependent app's Deployment.
+        if (app.dependsOn?.length) {
+          const toWait = name => getRunningPod(namespace, name)
+          await Promise.all(app.dependsOn.map(toWait))
+        }
 
-      if (uid) {
-        await apps.patchNamespacedDeployment(
-          name,
-          namespace,
-          resource,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          { headers: { 'Content-Type': 'application/merge-patch+json' } }
-        )
-        logger.success('Updated Deployment:', name)
-      } else {
-        await apps.createNamespacedDeployment(namespace, resource)
-        logger.success('Created Deployment:', name)
+        if (uid) {
+          await apps.patchNamespacedDeployment(
+            name,
+            namespace,
+            resource,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            { headers: { 'Content-Type': 'application/merge-patch+json' } }
+          )
+          logger.success('Updated Deployment:', name)
+        } else {
+          await apps.createNamespacedDeployment(namespace, resource)
+          logger.success('Created Deployment:', name)
+        }
+      } else if (resource.kind === 'Service') {
+        if (uid) {
+          await core.patchNamespacedService(
+            name,
+            namespace,
+            resource,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            { headers: { 'Content-Type': 'application/merge-patch+json' } }
+          )
+          logger.success('Updated Service:', name)
+        } else {
+          await core.createNamespacedService(namespace, resource)
+          logger.success('Created Service:', name)
+        }
+      } else if (resource.kind === 'Secret') {
+        if (uid) {
+          await core.patchNamespacedSecret(
+            name,
+            namespace,
+            resource,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            { headers: { 'Content-Type': 'application/merge-patch+json' } }
+          )
+          logger.success('Updated Secret:', name)
+        } else {
+          await core.createNamespacedSecret(namespace, resource)
+          logger.success('Created Secret:', name)
+        }
+      } else if (resource.kind === 'Ingress') {
+        if (uid) {
+          await net.patchNamespacedIngress(
+            name,
+            namespace,
+            resource,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            { headers: { 'Content-Type': 'application/merge-patch+json' } }
+          )
+          logger.success('Updated Ingress:', name)
+        } else {
+          await net.createNamespacedIngress(namespace, resource)
+          logger.success('Created Ingress:', name)
+        }
+      } else if (resource.kind === 'ConfigMap') {
+        if (uid) {
+          await core.patchNamespacedConfigMap(
+            name,
+            namespace,
+            resource,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            { headers: { 'Content-Type': 'application/merge-patch+json' } }
+          )
+          logger.success('Updated ConfigMap:', name)
+        } else {
+          await core.createNamespacedConfigMap(namespace, resource)
+          logger.success('Created ConfigMap:', name)
+        }
+      } else if (resource.kind === 'PriorityClass') {
+        await sched.createPriorityClass(resource)
+        logger.success('Created PriorityClass:', name)
       }
-    } else if (resource.kind === 'Service') {
-      if (uid) {
-        await core.patchNamespacedService(
-          name,
-          namespace,
-          resource,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          { headers: { 'Content-Type': 'application/merge-patch+json' } }
-        )
-        logger.success('Updated Service:', name)
-      } else {
-        await core.createNamespacedService(namespace, resource)
-        logger.success('Created Service:', name)
-      }
-    } else if (resource.kind === 'Secret') {
-      if (uid) {
-        await core.patchNamespacedSecret(
-          name,
-          namespace,
-          resource,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          { headers: { 'Content-Type': 'application/merge-patch+json' } }
-        )
-        logger.success('Updated Secret:', name)
-      } else {
-        await core.createNamespacedSecret(namespace, resource)
-        logger.success('Created Secret:', name)
-      }
-    } else if (resource.kind === 'Ingress') {
-      if (uid) {
-        await net.patchNamespacedIngress(
-          name,
-          namespace,
-          resource,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          { headers: { 'Content-Type': 'application/merge-patch+json' } }
-        )
-        logger.success('Updated Ingress:', name)
-      } else {
-        await net.createNamespacedIngress(namespace, resource)
-        logger.success('Created Ingress:', name)
-      }
-    } else if (resource.kind === 'ConfigMap') {
-      if (uid) {
-        await core.patchNamespacedConfigMap(
-          name,
-          namespace,
-          resource,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          { headers: { 'Content-Type': 'application/merge-patch+json' } }
-        )
-        logger.success('Updated ConfigMap:', name)
-      } else {
-        await core.createNamespacedConfigMap(namespace, resource)
-        logger.success('Created ConfigMap:', name)
-      }
-    } else if (resource.kind === 'PriorityClass') {
-      await sched.createPriorityClass(resource)
-      logger.success('Created PriorityClass:', name)
+    } catch (err) {
+      logger[logLevel](`Failed to apply ${resource.kind}:`, name)
+      logger.error(err.response?.body?.message || err)
+      if (logLevel === 'fatal') process.exit(1)
     }
-  } catch (err) {
-    const level = cfg.input.failFast ? 'fatal' : 'error'
-    logger[level](`Failed to apply ${resource.kind}:`, name)
-    logger.error(err.response?.body?.message || err)
-    if (level === 'fatal') process.exit(1)
   }
 }
 
@@ -159,14 +160,17 @@ export default async function apply (cfg) {
     }
   }
 
+  // Setup the applyResource function with the appropriate log level.
+  const applyResource = getApplyResource(cfg.input.failFast ? 'fatal' : 'error')
+
   // Apply top-level resources before app-level resources in case the apps
   // depend on them.
   const topLevelResources = resources.filter(r => !r.app)
   if (topLevelResources.length) await Promise.all(resources.map(applyResource))
 
   // Apply the app-level resources.
-  await Promise.all(Object.entries(cfg.app).map(async ([name]) => {
-    for (const resource of resources.filter(r => r.app.name === name)) {
+  await Promise.all(Object.entries(cfg.apps).map(async ([name]) => {
+    for (const resource of resources.filter(r => r.app?.name === name)) {
       await applyResource(resource)
     }
   }))
