54c365326935c10700429a4cf7b3d52ee8b62b7a
Ian Walter <pub@ianwalter.dev>
2021-02-18 22:14:16 -0500

    Adding pod delete logic

--- packages/kdot/lib/commands/cleanup.js
+++ packages/kdot/lib/commands/cleanup.js
@@ -1,4 +1,7 @@
 import { createLogger, chalk } from '@generates/logger'
+import prompt from '@generates/prompt'
+import { oneLine } from 'common-tags'
+import { kc, k8s } from '../k8s.js'
 import getPods from '../getPods.js'
 import emojis from '../emojis.js'
 
@@ -28,4 +31,33 @@ export default async function cleanup (cfg) {
     }
     if (pod.status.message) logger.log(pod.status.message)
   }
+
+  if (failedPods.length) {
+    if (cfg.input.prompt) {
+      try {
+        const question = oneLine`
+          Are you sure you want to delete these pods in the
+          ${chalk.yellow(cfg.namespace)} namespace of
+          ${chalk.yellow(kc.currentContext)}?
+        `
+        const response = await prompt.select(question)
+        process.stdout.write('\n')
+        if (response === 'No') process.exit(0)
+      } catch (err) {
+        logger.debug(err)
+        process.exit(0)
+      }
+
+      await Promise.all(failedPods.map(async pod => {
+        await k8s.client.delete({ ...pod, apiVersion: 'v1', kind: 'Pod' })
+      }))
+
+      logger.success('Cleaned up failed pods')
+    }
+  } else {
+    process.stdout.write('\n')
+    logger.info('No pods to cleanup')
+  }
+
+  process.stdout.write('\n')
 }
