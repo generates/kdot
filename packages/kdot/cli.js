78c8ddc247973f45e623ddbabe0b1302badbe24d
Ian Walter <pub@ianwalter.dev>
2021-02-22 11:11:05 -0500

    Updating cli config

--- packages/kdot/cli.js
+++ packages/kdot/cli.js
@@ -12,105 +12,105 @@ cli({
   usage: 'kdot [command] [apps] [options]',
   commands: {
     apply: {
-      execute: kdot.apply,
       description: `
         Create or update resources in a cluster for a given configuration
       `,
       options: {
         prompt: {
-          alias: 'p',
+          aliases: ['p'],
           description: `
             Whether to show a confirmation prompt before applying resources
           `,
           default: true
         },
         failFast: {
-          alias: 'f',
+          aliases: ['f'],
           description: 'Whether to exit on the first failure encountered',
           default: false
         },
-        wait: { alias: 'w' }
-      }
+        wait: { aliases: ['w'] }
+      },
+      run: kdot.apply
     },
     start: {
-      execute: kdot.start,
       description: `
         Facilitate local development by applying resources, streaming logs, and
         forwarding ports so that resources can be used locally
       `,
       options: {
         update: {
-          alias: 'u',
+          aliases: ['u'],
           description: `
             Whether to update resources before streaming logs and forwarding
             ports
           `
         }
-      }
+      },
+      run: kdot.start
     },
     forward: {
       aliases: ['fwd'],
-      execute: kdot.fwd,
       description: `
         Forward local ports to apps in the cluster for a given configuration
-      `
+      `,
+      run: kdot.fwd
     },
     logs: {
       aliases: ['log'],
-      execute: kdot.log,
       description: `
         Stream logs from apps in the cluster for a given configuration to stdout
-      `
+      `,
+      run: kdot.log
     },
     stop: {
-      execute: kdot.stop,
       description: `
         Stop forwarding ports and streaming logs when start was run in detached
         mode (in development)
-      `
+      `,
+      run: kdot.stop
     },
     show: {
-      execute: kdot.show,
       description: `
         Show a list of resources in the cluster for a given configuration that
         is configured by kdot
-      `
+      `,
+      run: kdot.show
     },
     scale: {
-      execute: kdot.scale,
       description: `
         Scale up or down the number of replicas for an app in the cluster (in
         development)
-      `
+      `,
+      run: kdot.scale
     },
     delete: {
       aliases: ['del'],
-      execute: kdot.del,
       description: `
         Delete a namespace or resources for a given app in the cluster for a
         given configuration
       `,
       options: {
         prompt: {
-          alias: 'p',
+          aliases: ['p'],
           description: `
             Whether to show a confirmation prompt before deleting resources
           `,
           default: true
         }
-      }
+      },
+      run: kdot.del
     },
     build: {
-      execute: kdot.build,
       description: `
         Build container images in the cluster using Kaniko for apps in a given
         configuration
       `,
       options: {
         timeout: {
-          alias: 't'
+          aliases: ['t']
         }
-      }
+      },
+      run: kdot.build
     },
     copy: {
       description: `
@@ -118,40 +118,42 @@ cli({
         cluster for a given configuration
       `,
       aliases: ['cp'],
-      execute: kdot.cp
+      run: kdot.cp
     },
     cleanup: {
       aliases: ['clean'],
-      execute: kdot.clean,
-      description: 'Delete failed pods in a namespace for a given configuration'
+      description: `
+        Delete failed pods in a namespace for a given configuration'
+      `,
+      run: kdot.clean
     },
     set: {
-      execute: kdot.set,
       description: 'Set a configuration value for a given configuration',
       options: {
         prop: {
           description: ''
         }
-      }
+      },
+      run: kdot.set
     },
     get: {
-      execute: kdot.get,
       description: 'Log a configuration value for a given configuration',
       options: {
         prop: {
           description: ''
         }
-      }
+      },
+      run: kdot.get
     }
   },
   options: {
     config: {
-      alias: 'c',
+      aliases: ['c'],
       description: 'The name of the config to use',
       default: 'personal'
     },
     ext: {
-      alias: 'e',
+      aliases: ['e'],
       description: 'Extend/override a config value using dot notation',
       default: {}
     }
