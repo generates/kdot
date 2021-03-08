e5b1b391fb74757fa8ead958fda647b23633ed58
Ian Walter <pub@ianwalter.dev>
2021-03-07 21:33:12 -0500

    Updating actions runner to v2.277.1

---
+++ .changeset/funny-gorillas-matter.md
@@ -0,0 +1,5 @@
+---
+"@generates/kdot-actions-runner": major
+---
+
+Updating actions runner to v2.277.1
--- packages/kdot-actions-runner/index.js
+++ packages/kdot-actions-runner/index.js
@@ -2,7 +2,7 @@ export default {
   namespace: 'ci',
   apps: {
     'actions-runner': {
-      image: { repo: 'generates/actions-runner', tag: 'v0.0.1' },
+      image: { repo: 'generates/actions-runner', tag: 'v2.277.1' },
       env: {
         NAME: {
           fieldRef: { fieldPath: 'metadata.name' }
