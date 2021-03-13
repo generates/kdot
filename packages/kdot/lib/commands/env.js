e30698e8c195d85ebf69e6bd84a5f945ef9e0424
Ian Walter <pub@ianwalter.dev>
2021-03-13 10:27:25 -0500

    Improvign root content for env command

--- packages/kdot/lib/commands/env.js
+++ packages/kdot/lib/commands/env.js
@@ -9,6 +9,8 @@ const globa = util.promisify(glob)
 const ignore = 'node_modules/**'
 const globOptions = { nosort: true, nodir: true, ignore, absolute: true }
 const paths = ['example.env', '*/example.env', '*/*/example.env']
+const inheritableHeader = '# Inheritable environment variables.'
+const inheritedHeader = '# Inherited environment variables.'
 
 export default async function env () {
   // Collect example.env files in the current directory and up to 2-levels deep.
@@ -37,14 +39,20 @@ export default async function env () {
 
         // Copy the root .env content into the new .env file as well.
         logger.log(`Appending root content to ${relativeDot}`)
-        await fs.appendFile(dot, `\n# From root .env\n${rootContent}`)
+
+        await fs.appendFile(dot, `\n${rootContent}`)
       }
     }
 
     // If the file is the root .env file, save it's content so it can be
     // appended to nested .env files.
     if (dir === process.cwd()) {
-      rootContent = await fs.readFile(file, 'utf8')
+      rootContent = await fs.readFile(dot, 'utf8')
+      const headerIndex = rootContent.indexOf(inheritableHeader)
+      rootContent = rootContent.substring(headerIndex)
+      const blankLineIndex = rootContent.indexOf('\n\n') + 1
+      rootContent = rootContent.substring(0, blankLineIndex)
+      rootContent = rootContent.replace(inheritableHeader, inheritedHeader)
       logger.debug('Root content', rootContent)
     }
   }
