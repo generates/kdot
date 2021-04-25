899585212a5d4e461f24b0d82e209d9aad4a2ae5
Ian Walter <pub@ianwalter.dev>
2021-04-25 17:03:59 -0400

    Fleshing out rollout logic

--- packages/kdot/lib/commands/roll.js
+++ packages/kdot/lib/commands/roll.js
@@ -2,6 +2,7 @@ import { createLogger } from '@generates/logger'
 import { k8s } from '../k8s.js'
 import configure from '../configure/index.js'
 import getTargetApps from '../getTargetApps.js'
+import getPods from '../getPods.js'
 
 const logger = createLogger({ level: 'info', namespace: 'kdot.roll' })
 
@@ -14,8 +15,12 @@ export default async function roll (input) {
     const replicas = app.replicas || 1
 
     // Get pods.
+    const pods = await getPods({ })
 
     for (let i = 0; i <= replicas - 1; i++) {
+      // Check if the current pod has an old config hash.
+      const pod = pods[i]
+
       // If there are multiple pods, delete one.
 
       // Otherwise scale the replicas to 2.
@@ -24,6 +29,8 @@ export default async function roll (input) {
 
       // If there was only 1 pod before, delete it now that the new one is
       // ready.
+
+      // Scale the replicas back down to 1 if necessary.
     }
   }
 }
